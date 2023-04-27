187b9e1325b29c6f9a58ffa75aa8d0e9d506a6d4
Deniz Seyrekbasan <seyrekbasand@gmail.com>
2023-04-28 00:40:56 +0300

    Add project files.

---
+++ Matematik/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matematik
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Toplama : 1");
+            Console.WriteLine("Çıkarma : 2");
+            Console.WriteLine("Carpma : 3");
+            Console.WriteLine("Bolme : 4");
+            
+            Console.Write("İşlem türünü seçiniz : ");
+            int secim = int.Parse(Console.ReadLine());
+
+            if (secim == 1)
+            {
+                Console.WriteLine("Sayi 1 : ");
+                int s1 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Sayi 2 :");
+                int s2 = int.Parse(Console.ReadLine());
+
+                DortIslem yeni = new DortIslem();
+                yeni.Topla(s1, s2);
+                
+            }
+
+            Console.Read();
+
+        }
+    }
+}
---
+++ Metotlar/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metotlar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string urunAdi = "Elma";
+            double fiyati = 15;
+            string aciklama = "Amasya elması";
+
+            string[] meyveler = new string[] { };
+
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya elması";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakır karpuzu";
+
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+            sepetManager.Ekle2("armut", "yesil armut", 12,50);
+            sepetManager.Ekle2("elma", "yesil elma", 12,50);
+            sepetManager.Ekle2("kiraz", "kırmızı kiraz", 12,50);
+
+            Console.Read();
+
+
+        }
+    }
+}
